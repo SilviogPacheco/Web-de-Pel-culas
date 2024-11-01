53f23ad4023e7f6582b76426e8b657467b61becd
SilviogPacheco <megaboy93mile@gmail.com>
2024-11-01 19:54:59 -0300

    agrego cards y diseño

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,51 @@
-console.log(tempData);
+const createCard = (ele) => {
+  const cardDiv = document.createElement("div");
+
+  const titleCard = document.createElement("h3");
+  const imageCard = document.createElement("img");
+  const detailsDiv = document.createElement("div");
+  const yearCard = document.createElement("h3");
+  const directorCard = document.createElement("h3");
+  const durationCard = document.createElement("h3");
+  const genreCard = document.createElement("h3");
+  const rateCard = document.createElement("h3");
+
+  titleCard.innerHTML = ele.title;
+  imageCard.src = ele.poster;
+  yearCard.innerHTML = `Año: ${ele.year}`;
+  directorCard.innerHTML = `Director: ${ele.director}`;
+  durationCard.innerHTML = `Duración: ${ele.duration}`;
+  rateCard.innerHTML = `Rating: ${ele.rate}`;
+
+  ele.genre.forEach((e) => {
+    const genreElement = document.createElement("span");
+    genreElement.innerHTML = e;
+    genreElement.classList.add("genre-item");
+    genreCard.appendChild(genreElement);
+  });
+
+  cardDiv.classList.add("card");
+  titleCard.classList.add("title-card");
+  imageCard.classList.add("image-card");
+  detailsDiv.classList.add("details");
+  genreCard.classList.add("genre-card");
+
+  detailsDiv.appendChild(yearCard);
+  detailsDiv.appendChild(directorCard);
+  detailsDiv.appendChild(durationCard);
+  detailsDiv.appendChild(rateCard);
+  detailsDiv.appendChild(genreCard);
+
+  cardDiv.appendChild(titleCard);
+  cardDiv.appendChild(imageCard);
+  cardDiv.appendChild(detailsDiv);
+
+  return cardDiv;
+};
+
+const cards = tempData.map((elem) => createCard(elem));
+
+cards.forEach((ele) => {
+  const container = document.getElementById("cards-container");
+  container.appendChild(ele);
+});
